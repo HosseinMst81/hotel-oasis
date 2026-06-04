ce8d839d3f48e01a35662acccc9a3292fc15b58b
HosseinMst81 <hobinazire@yahoo.com>
2026-06-04 19:42:35 +0330

    Defin all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import styled from "styled-components";
-import DesignTokensTestDashboard from "./components/test/DesignTokensTestDashboard";
+import { Route, Routes } from "react-router";
 import GlobalStyles from "./styles/GlobalStyles";
-const H1 = styled.h1`
-  font-size: var(--text-4xl);
-  color: var(--color-brand-primary);
-  font-family: var(--font-base);
-`;
-
-const H2 = styled.h2`
-  font-size: var(--text-2xl);
-  color: var(--color-text-primary);
-  font-family: var(--font-primary);
-`;
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <H1>The Wild Oasis</H1>
-      <H2> Welcome to our hotel!</H2>
-      <DesignTokensTestDashboard></DesignTokensTestDashboard>
+      {/* <QueryClientProvider client={queryClient}> */}
+      {/* <ThemeProvider theme={theme}> */}
+        <GlobalStyles />
+          {/* <AuthProvider> */}
+            {/* <SettingsProvider> */}
+              <Routes>
+                <Route path="/login" element={<>Login</>}/>
+                {/* <Route element={<AppLayout />}> */}
+                  <Route path="/dashboard" element={<>Dashboard</>}/>
+                  <Route path="/bookings" element={<>Bookings</>}/>
+                  <Route path="/bookings/new" element={<>NewBooking</>}/>
+                  <Route path="/bookings/:bookingId" element={<>BookingDetail</>}/>
+                  <Route path="/check-in/:bookingId" element={<>CheckIn</>}/>
+                  <Route path="/check-out/:bookingId" element={<>CheckOut</>}/>
+                  <Route path="/cabins" element={<>Cabins</>}/>
+                  <Route path="/cabins/new" element={<>CabinForm</>}/>
+                  <Route path="/cabins/:cabinId/edit" element={<>CabinForm</>}/>
+                  <Route path="/cancelled" element={<>CancelledBookings</>}/>
+                  <Route path="/settings" element={<>Settings</>}/>
+                  <Route path="/profile" element={<>Profile</>}/>
+                  <Route path="/users" element={<>Users</>}/>
+                  <Route path="/users/new" element={<>UserForm</>}/>
+                  <Route path="*" element={<>NotFound</>}/>
+                {/* </Route> */}
+              </Routes>
+            {/* </SettingsProvider> */}
+          {/* </AuthProvider> */}
+        <Toaster position="top-right" />
+      {/* </ThemeProvider> */}
+    {/* </QueryClientProvider> */}
     </>
   );
 }
