73e86b0430edcf1619c204db0be831a24e09fdb5
HosseinMst81 <hobinazire@yahoo.com>
2026-06-10 12:32:16 +0330

    Add: Inline Component

--- src/design/components/test/Test.tsx
+++ src/design/components/test/Test.tsx
@@ -1,9 +1,19 @@
+import { Button } from "../../primitives";
+import Inline from "../../primitives/Inline/inline";
 import Loading from "../../primitives/Loading/Loading";
 
 const Test = () => {
   return (
     <div>
       <Loading />
+      <Inline spacing={2} justify="space-around">
+        <Button colorScheme="primary">Primary</Button>
+        <Button colorScheme="secondary">Secondary</Button>
+        <Button colorScheme="error">Error</Button>
+        <Button colorScheme="info">Info</Button>
+        <Button colorScheme="dark">Dark</Button>
+        <Button colorScheme="light">Light</Button>
+      </Inline>
     </div>
   );
 };
